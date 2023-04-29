3a68816d3fc7904c4187b76e2dd3af5ce99dcdd5
edison <edisoncardenas151999@gmail.com>
2023-04-29 14:06:23 -0400

    done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-let i = 0
-  console.log("Hello Rigo from the console!");
+  document.querySelector('#btn').addEventListener('click',() =>{
+    document.querySelector('#excuse').innerHTML = randomExcuseGenerator();
+  }
+  )
+
 };
+
+const randomExcuseGenerator = () =>{
+let who = ['The dog','My grandma','His turtle','My bird'];
+let action = ['ate','peed','crushed','broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+let random1 = Math.floor(Math.random()* who.length)
+let random2 = Math.floor(Math.random() * action.length)
+let random3 = Math.floor(Math.random() * what.length)
+let random4 = Math.floor(Math.random() * when.length)
+
+return who[random1] + " " + action[random2] + " " + what[random3] + " "+ when[random4]
+}
